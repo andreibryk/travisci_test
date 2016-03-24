bbac629480b33b76b8c4b325dadb24855d2a6427
ANDREI BRYK <andrei.bryk@gmail.com>
2016-03-24 19:40:44 +0300

    fix

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,6 +2,7 @@
 
 openssl aes-256-cbc -K $encrypted_bd69831a7c24_key -iv $encrypted_bd69831a7c24_iv -in snapcarttest.pem.enc -out deployment/snapcarttest.pem -d
 
-if [ "${TRAVIS_BRANCH:(-3)}" = "_VH" ]; then
-    cap production deploy
+if [[ $TRAVIS_BRANCH == *"My _VH"* ]]
+then
+  cap production deploy
 fi
