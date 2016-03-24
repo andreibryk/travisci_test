740132e0c4fffd2c20cc334a65465a3460b0f209
ANDREI BRYK <andrei.bryk@gmail.com>
2016-03-24 13:21:09 +0300

    add deploy to build

---
+++ deployment/stages/production.rb
@@ -0,0 +1,12 @@
+# server-based syntax
+# ======================
+server '54.251.184.132', user: 'ubuntu', roles: %w{app}
+
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
