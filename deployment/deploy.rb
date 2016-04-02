92f9763a222edb43502d71dbe9e4bc13636d2a77
ANDREI BRYK <andrei.bryk@gmail.com>
2016-04-02 21:52:33 +0300

    composer settings

--- deployment/deploy.rb
+++ deployment/deploy.rb
@@ -28,7 +28,7 @@ set :ssh_options, {
   keys: ["deployment/snapcarttest.pem"]
 }
 
-set :composer_options,  "--verbose"
+set :composer_install_flags, '--no-interaction'
 set :symfony_directory_structure, 2
 set :sensio_distribution_version, 4
 
